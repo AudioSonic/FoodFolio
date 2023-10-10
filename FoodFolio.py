0e1bacddf6e1476c101411ed295ed82f7adde5dd
Alexander Stach <stach.alex@yahoo.de>
2023-10-10 20:39:26 +0200

    Komplette Überarbeitung

--- FoodFolio.py
+++ FoodFolio.py
@@ -1,10 +1,27 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
+import customtkinter as ctk
 
-class MyApp(App): 
-    def build(self):
-        return Label(text="FoodFolio")
-    
-if __name__ == "__main__":
-    MyApp().run()
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
+
+root = ctk.CTk()
+root.geometry("1920x1080")
+
+frame = ctk.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = ctk.CTkLabel(master=frame, text="FoodFolio")
+label.pack(pady=12, padx=10)
+
+entry1 = ctk.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = ctk.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = ctk.CTkButton(master=frame, text ="Login")
+button.pack(pady=12, padx=10)
+
+checkbox = ctk.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
